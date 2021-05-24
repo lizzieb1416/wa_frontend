724449848a99296ca18df94a5648ae7f0e3b837c
lisetTablet <lizethbastidas@gmail.com>
2021-05-24 18:10:12 +0200

    bug fixed

--- frontend-doc.md
+++ frontend-doc.md
@@ -12,3 +12,10 @@ copy the <link /> from CSS and paste it in public > index.html
 Google fonts: 
 https://fonts.google.com/?query=mont
 copy the <link /> from CSS and paste it in public > index.html
+
+## STEPS AFTER CLONING REPOSITORY FROM GITHUB 
+It is possible to find a problem with npm when trying to start the project, this is maybe due to the repository 
+being a submodule.
+
+npm update 
+npm start
--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends React.Component {
         this.setState({
            activeItem:{
           id:null, 
-          title:'',
+          description:'',
           completed:false,
         }
         })
@@ -202,7 +202,7 @@ class App extends React.Component {
                  <form onSubmit={this.handleSubmit}  id="form">
                     <div className="flex-wrapper">
                         <div style={{flex: 6}}>
-                            <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Add task.." />
+                            <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.description} type="text" name="title" placeholder="Add task.." />
                          </div>
 
                          <div style={{flex: 1}}>
